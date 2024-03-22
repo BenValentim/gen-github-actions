854ae57b583e43182587f8bb4dd7b1c9c0451398
Ben <ben.valentim@gmail.com>
2024-03-22 11:25:06 -0300

    feat: fill env vars prefix & customized vars

---
+++ README.md
@@ -0,0 +1,33 @@
+# GitHub Actions Generator
+The GitHub Actions Generator is a tool that allows you to automatically generate GitHub Actions configuration files based on customizable templates provided by the user. With this project, you can automate repetitive tasks and set up custom workflows according to your project's needs.
+
+## Installation
+To start using the GitHub Actions Generator, you need to install the npm or yarn package. Run the following command in your terminal:
+
+```bash
+npm install gen-github-actions
+```
+or
+```bash
+yarn install gen-github-actions
+```
+
+## How to Use
+After installing the package, you can use the generate command followed by an array of environments for which the files will be generated and whether to use the variables from your env-sample and env-ci=sample. For example:
+
+genActions.generate([
+{
+"<WORKSPACE>": "dev",
+"<SERVER_PATH>": "/test/url/",
+"<TASK_NAME>": "fill env dev"
+},
+{
+"<WORKSPACE>": "main",
+"<SERVER_PATH>": "/test-main/url/",
+"<TASK_NAME>": "fill env main"
+}
+], true, true);
+
+## Contribution
+If you encounter any issues or have suggestions for improvement, feel free to open an issue in this repository. We welcome contributions from all levels of experience.
+
--- index.ts
+++ index.ts
@@ -4,6 +4,37 @@ import path from 'path';
 const from = '.github/models';
 const destiny = '.github/workflows';
 
+const validateWorkspaces = (workspaces: { [key: string]: string }[]): boolean => {
+  const allKeys = new Set<string>();
+
+  for (const workspace of workspaces) {
+    const keys = Object.keys(workspace);
+    if (!keys.includes('<WORKSPACE>')) {
+      return false;
+    }
+
+    for (const key of keys) {
+      if (isValidParameter(key)) {
+        allKeys.add(key);
+      } else {
+        return false;
+      }
+    }
+  }
+
+  for (const workspace of workspaces) {
+    if (!Array.from(allKeys).every(key => Object.keys(workspace).includes(key))) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const isValidParameter = (parameter: string): boolean => {
+  return parameter.startsWith('<') && parameter.endsWith('>');
+}
+
 function processEnvFile(modifiedLines: string[], envFileName: string, envType: string): string[] {
   const envFileContent = fs.readFileSync(envFileName, 'utf-8');
   const envLines = envFileContent.split(/\r?\n/);
@@ -11,12 +42,22 @@ function processEnvFile(modifiedLines: string[], envFileName: string, envType: s
   let foundIndex = -1;
 
   modifiedLines.forEach((line, index) => {
-    if (line.includes('rm .env-')) {
+    if (line.includes('rm .env')) {
       foundIndex = index;
-    } else if (foundIndex !== -1 && index === foundIndex + 2) {
+    }
+
+    if (foundIndex !== -1 && index === foundIndex + 2) {
       envLines.forEach(envLine => {
-        const varName = envLine.split('=')[0];
-        modifiedLines.splice(index, 0, `          && echo ${varName}=\${{ vars.${varName} }} >> .env-${envType}`);
+        if (envLine.includes('#')) {
+          return;
+        }
+
+        const varPrefix = envLine.split('.');
+
+        if (varPrefix.length > 1) {
+          const varName = varPrefix[1].split('=')[0];
+          modifiedLines.splice(index, 0, `          && echo ${varName}=\${{ ${varPrefix[0]}.${varName} }} >> .env-${envType}`);
+        }
       });
       foundIndex = -1;
     }
@@ -25,12 +66,15 @@ function processEnvFile(modifiedLines: string[], envFileName: string, envType: s
   return modifiedLines;
 }
 
-function fillLines(lines: string[], serverPath: string, envType: string, useEnvSample: boolean, useCiSample: boolean): string[] {
+function fillLines(lines: string[], envType: string, values: any, useEnvSample: boolean, useCiSample: boolean): string[] {
   let modifiedLines = lines.map((line: string) => {
-    let modLines = line.replace(/<WORKSPACE>/g, envType);
-    modLines = line.replace(/<PATH>/g, serverPath);
+    let modLine = line;
 
-    return modLines
+    Object.keys(values).forEach((key: string) => {
+      modLine = modLine.replace(new RegExp(`${key}`, 'g'), values[key]);
+    });
+
+    return modLine
   });
 
   if (useCiSample) {
@@ -45,25 +89,47 @@ function fillLines(lines: string[], serverPath: string, envType: string, useEnvS
 }
 
 const genActions: any = {
-  generate: (workspaces: string[], serverPath: string, useEnvSample: boolean, useCiSample: boolean) => {
+  generate: (params: { [key: string]: string }[], useEnvSample: boolean, useCiSample: boolean) => {
     try {
+      if (!validateWorkspaces(params)) {
+        console.error('Error: Invalid parameters in workspaces array');
+        return;
+      }
+
       const files = fs.readdirSync(from);
 
       files.forEach((modelName: string) => {
         const fileContent = fs.readFileSync(`${from}/${modelName}`, 'utf-8');
         const lines = fileContent.split(/\r?\n/);
 
-        for (let index = 0; index < workspaces.length; index++) {
-          const envType = workspaces[index];
-          const modifiedLines = fillLines(lines, serverPath, envType, useEnvSample, useCiSample);
+        params.forEach((values: { [key: string]: string }) => {
+          const envType = values['<WORKSPACE>'];
+          const modifiedLines = fillLines(lines, envType, values, useEnvSample, useCiSample);
           const modifiedContent = modifiedLines.join('\n');
           const destinyFile = path.join(destiny, `${envType}-${modelName}`);
           fs.writeFileSync(destinyFile, modifiedContent);
           console.log(`Workflow file created at ${destinyFile}`);
-        }
+        });
       });
     } catch (error) {
       console.error('Error creating workflow file:', error);
     }
   }
 }
+
+genActions.generate([
+  {
+    "<WORKSPACE>": "dev",
+    "<SERVER_PATH>": "/test/url/",
+    "<ENV_TASK_NAME>": "fill env dev",
+    "<TEST_TASK_NAME>": "test dev",
+    "<CREDENTIALS>": "1234"
+  },
+  {
+    "<WORKSPACE>": "main",
+    "<SERVER_PATH>": "/test-main/url/",
+    "<ENV_TASK_NAME>": "fill env main",
+    "<TEST_TASK_NAME>": "test main",
+    "<CREDENTIALS>": "5678"
+  }
+], true, true);
